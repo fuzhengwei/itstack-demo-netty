ec2fb8d6031aadeba45a61644f69e0d7534071c9
fuzhengwei <184172133@qq.com>
2019-09-03 21:27:44 +0800

    付政委 bugstack虫洞栈 更新2-09 netty集群部署，跨服务通信

--- itstack-demo-netty-2-05/src/test/java/org/itstack/demo/test/ApiTest.java
+++ itstack-demo-netty-2-05/src/test/java/org/itstack/demo/test/ApiTest.java
@@ -1,12 +1,18 @@
 package org.itstack.demo.test;
 
+import io.netty.util.NettyRuntime;
+import io.netty.util.internal.SystemPropertyUtil;
+
 /**
  * create by fuzhengwei3 on 2019/8/9
  */
 public class ApiTest {
 
     public static void main(String[] args) {
-        System.out.println("");
+        int max = Math.max(1, SystemPropertyUtil.getInt(
+                "io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
+
+        System.out.println(max);
     }
 
 }
