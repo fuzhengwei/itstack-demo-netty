5585676b5dabdda69fefc887610ee6f600f0c0c3
fuzhengwei <184172133@qq.com>
2019-10-06 22:56:18 +0800

    公众号：bugstack虫洞栈 | 基于Netty搭建WebSocket，模仿微信聊天页面

--- itstack-demo-netty-2-05/src/test/java/org/itstack/demo/test/ApiTest.java
+++ itstack-demo-netty-2-05/src/test/java/org/itstack/demo/test/ApiTest.java
@@ -9,6 +9,7 @@ import io.netty.util.internal.SystemPropertyUtil;
 public class ApiTest {
 
     public static void main(String[] args) {
+        System.out.println("hi 微信公众号：bugstack虫洞栈");
         int max = Math.max(1, SystemPropertyUtil.getInt(
                 "io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
 
