68d45ce60ce60f618a5534070656470e12841f8d
付政委 <fuzhengwei@users.noreply.github.com>
2019-11-14 15:19:40 +0800

    公众号：bugstack虫洞栈 | Netty传输文件、分片发送、断点续传
    公众号：bugstack虫洞栈 | netty案例，netty4.1中级拓展篇四《Netty传输文件、分片发送、断点续传》

--- itstack-demo-netty-2-04/src/test/java/org/itstack/demo/test/NettyServerTest.java
+++ itstack-demo-netty-2-04/src/test/java/org/itstack/demo/test/NettyServerTest.java
@@ -11,7 +11,7 @@ import org.itstack.demo.netty.server.NettyServer;
 public class NettyServerTest {
 
     public static void main(String[] args) {
-        System.out.println("hi 微信公众号：bugstack虫洞栈");
+        System.out.println("hi 微信公众号：bugstack虫洞栈 | 欢迎关注&获取更多专题案例");
         //启动服务
         new NettyServer().bing(7397);
     }
